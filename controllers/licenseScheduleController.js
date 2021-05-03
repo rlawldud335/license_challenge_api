e7a97c6f9ff0269cc1463b0eb9815069bf282dc8
dalbikim <dalbikim@naver.com>
2021-05-04 05:34:22 +0900

    자격증, 자격증스케줄 조회
    자격증, 자격증스케줄 조회

---
+++ controllers/licenseScheduleController.js
@@ -0,0 +1,15 @@
+import { mysqlconn, mgconn } from "../db";
+const licenseScheduleQuery = require("../queries/licenseScheduleQuery");
+
+export const getLicenseSchedule = async (req, res) => {
+    let { scheduleId } = req.params;
+    try {
+      const conn = await mysqlconn.getConnection(async (conn) => conn);
+      const data = await conn.query(licenseScheduleQuery.getLicenseSchedule, [scheduleId]);
+      const rows = data[0];
+      return res.json(rows[0]);
+    } catch(error) {
+      console.log(error);
+      return res.status(500).json(error);
+    }
+};
