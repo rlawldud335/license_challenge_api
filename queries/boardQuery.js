7d8163773a6b9b4c88613e852c387b60937e44ac
jihy2on <58081304+jihy2on@users.noreply.github.com>
2021-05-04 06:32:16 +0900

    Board_Comment 생성, 삭제, 조회 추가

--- queries/boardQuery.js
+++ queries/boardQuery.js
@@ -2,3 +2,7 @@ export const getAllBoard = "SELECT * FROM board";
 export const getBoard = "SELECT * FROM board WHERE boardId = ?";
 export const createBoard = "INSERT INTO board (boardId, userId, category, title, content, createDt, image, editDt, reportCnt) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)";
 export const deleteBoard = "DELETE FROM board where boardId = ?";
+
+export const getComment = "SELECT * FROM board_comment WHERE boardId = ?";
+export const createComment = "INSERT INTO board_comment (commentId, boardId, userId, content, createDt, editDt, level, precedingComment) VALUES (?, ?, ?, ?, ?, ?, ?, ?)";
+export const deleteComment = "DELETE FROM board_comment where boardId = ? AND commentId = ?";
--- routes/boardRouter.js
+++ routes/boardRouter.js
@@ -15,4 +15,12 @@ boardRouter.post("/create", boardController.createBoard);
 //delete board
 boardRouter.delete("/delete/:boardId", boardController.deleteBoard);
 
+
+
+//create comment
+boardRouter.post("/read/:boardId/create", boardController.createComment);
+
+//delete comment
+boardRouter.delete("/delete/:boardId/comment/:commentId", boardController.deleteComment);
+
 export default boardRouter;
