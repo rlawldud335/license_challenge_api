45a50dbaf28477770322d284fdefaf4de57a60ce
김지영 <rlawldud335@naver.com>
2021-05-07 01:44:31 +0900

    PRODUCTION_Test

--- src/init.js
+++ src/init.js
@@ -9,8 +9,8 @@ const PORT = process.env.PORT || 3000;
 const hadleListening = () =>
   console.log(
     process.env.PRODUCTION
-      ? `Listening on: https://license-challenge.herokuapp.com:${PORT}`
-      : `Listening on: http://localhost:${PORT}`
+      ? ` ${process.env.PRODUCTION} Listening on: https://license-challenge.herokuapp.com:${PORT}`
+      : ` ${process.env.PRODUCTION} Listening on: http://localhost:${PORT}`
   );
 
 app.listen(PORT, hadleListening);
