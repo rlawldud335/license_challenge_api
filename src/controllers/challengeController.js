a39fc0662256d9180fdbeb6ac96dbc48d323721b
hyejung-Kim <65945647+hyejung-Kim@users.noreply.github.com>
2021-05-21 21:28:04 +0900

    내 챌린지 목록 조회, 챌린지 검색
    내 챌린지 목록 조회, 챌린지 검색

--- src/controllers/challengeController.js
+++ src/controllers/challengeController.js
@@ -1,6 +1,50 @@
 import { mysqlConn } from "../db";
 const challengeQuery = require("../queries/challengeQuery");
 
+<<<<<<< Updated upstream
+=======
+export const searchChallenge = async (req, res) => {
+  let { keyword } = req.params;
+  try {
+    await mysqlConn(async (conn) => {
+      const query = challengeQuery.searchChallenge;   
+      const [data, schema] = await conn.query(query,[keyword]);
+      return res.status(200).json(data);     
+    });
+  } catch (err) {
+    console.log(err);
+    return res.status(500).json(err);
+  }
+};
+
+export const getOngoingChallenge = async (req, res) => {
+  const userId = req.user.userId;
+  try {
+    await mysqlConn(async (conn) => {
+      const query = challengeQuery.getOngoingChallenge;   
+      const [data, schema] = await conn.query(query,[userId]);
+      return res.status(200).json(data);     
+    });
+  } catch (err) {
+    console.log(err);
+    return res.status(500).json(err);
+  }
+};
+
+export const getEndedChallenge = async (req, res) => {
+  const userId = req.user.userId;
+  try {
+    await mysqlConn(async (conn) => {
+      const query = challengeQuery.getEndedChallenge;   
+      const [data, schema] = await conn.query(query,[userId]);
+      return res.status(200).json(data);     
+    });
+  } catch (err) {
+    console.log(err);
+    return res.status(500).json(err);
+  }
+};
+>>>>>>> Stashed changes
 
 export const getCategoryChallenge = async (req, res) => {
   try {
@@ -43,6 +87,7 @@ export const getChallenge = async (req, res) => {
 };
 
 export const createChallenge = async (req, res) => {
+<<<<<<< Updated upstream
   const body = req.body;
   const user = req.user.userId;
   const chgStartDt = new Date(body.chgStartDt);
@@ -54,7 +99,20 @@ export const createChallenge = async (req, res) => {
   console.log("s3 title이미지 경로 :", challengeTitleImage.location);
   console.log("s3 good이미지 경로 :", goodProofImage.location);
   console.log("s3 bad이미지 경로 :", badProofImage.location);
+=======
+>>>>>>> Stashed changes
   try {
+    const body = req.body;
+    const userId = req.user.userId;
+    const chgStartDt = new Date(body.chgStartDt);
+    const chgEndDt = new Date(body.chgEndDt);
+    const challengeTerm = Math.ceil((chgEndDt.getTime()-chgStartDt.getTime())/(1000*3600*24)) * body.proofCountOneDay;
+    const challengeTitleImage = req.files["challengeTitleImage"][0];
+    const goodProofImage = req.files["goodProofImage"][0];
+    const badProofImage = req.files["badProofImage"][0];
+    console.log("s3 title이미지 경로 :", challengeTitleImage.location);
+    console.log("s3 good이미지 경로 :", goodProofImage.location);
+    console.log("s3 bad이미지 경로 :", badProofImage.location);
     await mysqlConn(async (conn) => {
       const [data, schema] = await conn.query(challengeQuery.createChallenge, [
         body.challengeTitle,
