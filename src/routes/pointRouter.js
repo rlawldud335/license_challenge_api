248385aeba9cd9230b3080ba94e931d356d0eee8
hyejung-Kim <65945647+hyejung-Kim@users.noreply.github.com>
2021-05-23 02:17:22 +0900

    포인트 적립
    포인트 인앱 적립(충전 제외)

--- src/routes/pointRouter.js
+++ src/routes/pointRouter.js
@@ -1,19 +1,22 @@
 import express from "express";
+import routes from "../routes";
 import {
     chargePoint,
     getPoint,
     usePoint,
     withdrawPoint,
-    getPointHistory
+    getPointHistory,
+    earnPoint
 } from "../controllers/pointController";
 
 const pointRouter = express.Router();
 
-pointRouter.get("/history", getPointHistory);
 pointRouter.get("/", getPoint);
+pointRouter.get("/history", getPointHistory);
 pointRouter.post("/charge", chargePoint);
 pointRouter.post("/withdraw", withdrawPoint);
 pointRouter.post("/payment", usePoint);
+pointRouter.post("/earn", earnPoint);
 
 
 export default pointRouter;
