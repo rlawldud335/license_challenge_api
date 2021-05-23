3f57d0a6aa2f88ec4b1aa0848fad75c5b3d4fdeb
dalbikim <dalbikim@naver.com>
2021-05-23 21:35:03 +0900

    자격증 일정 조회 완성
    로그인 된 사용자가 참여중인 챌린지의 자격증 일정

--- src/routes/licenseScheduleRouter.js
+++ src/routes/licenseScheduleRouter.js
@@ -1,9 +1,11 @@
 import express from "express";
 import routes from "../routes";
-import { getLicenseSchedule } from "../controllers/licenseScheduleController";
+import { getLicenseSchedule, getMySchedule } from "../controllers/licenseScheduleController";
 
 const licenseScheduleRouter = express.Router();
 
+// 로그인 된 사용자가 참여한 챌린지의 자격증 일정 조회
+licenseScheduleRouter.get(routes.mySchedule, getMySchedule);
 // read Detail licenseSchedule
 licenseScheduleRouter.get(routes.licenseSchedule, getLicenseSchedule);
 
